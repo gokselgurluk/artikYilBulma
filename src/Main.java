ec09d9f71a9def7d51196530f1d44c4b840cac78
göksel gürlük <gurlukgoksel@gmail.com>
2024-02-20 14:31:57 +0300

    koşul ifadesi düzenlendi

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,16 @@ public class Main {
         year = userInput.nextInt();
         //yıl bilgisini aldık
         if (year >= 0) {
-            if (year % 4 == 0 && year % 400 == 0) {
-                //  yıl bilgisi 100 un aktı olmayan yıllar için 4 e
-                //  100 katı olan yıllar için 400e bölünüp bölünmedigine baktık
-                System.out.println(year + " Bir artık yıldır: ");
-                // sonucu kullanıcı görmesi için ekrana bastırdık
-            } else {
-                System.out.print(year + " Bir artık yıl degildir: ");
+            if (year % 4 == 0) {
+                if (year % 100 == 0 && year % 400 == 0) {
+                    //  yıl bilgisi 100 un aktı olmayan yıllar için 4 e
+                    //  100 katı olan yıllar için 400e bölünüp bölünmedigine baktık
+                    System.out.println(year + " Bir artık yıldır: ");
+                    // sonucu kullanıcı görmesi için ekrana bastırdık
+                } else {
+                    System.out.print(year + " Bir artık yıl degildir: ");
 
+                }
             }
         }
         else {
