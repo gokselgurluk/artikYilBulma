cdeb99b7ac8d55dc4c7bb75221470951fbbcb971
göksel gürlük <gurlukgoksel@gmail.com>
2024-02-20 13:44:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        year = userInput.nextInt();
+        //yıl bilgisini aldık
+        if (year >= 0) {
+            if (year % 4 == 0 && year % 400 == 0) {
+                //  yıl bilgisi 100 un aktı olmayan yıllar için 4 e
+                //  100 katı olan yıllar için 400e bölünüp bölünmedigine baktık
+                System.out.println(year + " Bir artık yıldır: ");
+                // sonucu kullanıcı görmesi için ekrana bastırdık
+            } else {
+                System.out.print(year + " Bir artık yıl degildir: ");
+
+            }
+        }
+        else {
+            System.out.println("Yıl bilgisi 0 dan küçük olamaz");
+        }
+    }
+}
